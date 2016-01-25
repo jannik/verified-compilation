a7fc44eda8da6fefa0ab38226eddabe492dc0071
Mikkel Kragh Mathiesen <ntr859@alumni.ku.dk>
2016-01-26 00:30:49 +0100

    Add code to appendix.

--- CodeExtractor.hs
+++ CodeExtractor.hs
@@ -1,6 +1,8 @@
 module CodeExtractor where
 
 import Data.List
+import Control.Monad
+import System.Directory
 
 type Code = [String]
 
@@ -32,3 +34,14 @@ extractAll = do
     extract "code/totality-hoas-bruijn.elf" "trans-hb-exists'/app" "report/code-totality-hb-app.tex"
     extract "code/totality-hoas-bruijn.elf" "move-to-head" "report/code-totality-hb-move-to-head.tex"
     extract "code/soundness-bruijn-stack.elf" "%reduces" "report/code-soundness-bs.tex"
+
+appendixCode :: [(String, [String])] -> String
+appendixCode = unlines . concat . map f
+    where
+        f (fileName, contents) = ["\\subsubsection{" ++ fileName ++ "}", "\\begin{Verbatim}[fontsize=\\tiny]"] ++ contents ++ ["\\end{Verbatim}"]
+
+createAppendix :: IO ()
+createAppendix = do
+    fileNames <- filter ((== "fle.") . take 4 . reverse) . lines <$> readFile "code/sources.cfg"
+    tuples <- sequence [readFile ("code/" ++ fileName) >>= \contents -> return (fileName, lines contents) | fileName <- fileNames]
+    writeFile "report/code-appendix.tex" $ appendixCode tuples
