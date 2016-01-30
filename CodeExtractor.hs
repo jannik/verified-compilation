d7e609bb69a90acb9938a7b6858f883164711b3b
Mikkel Kragh Mathiesen <ntr859@alumni.ku.dk>
2016-01-30 12:20:25 +0100

    Extract example Tex code from Twelf code.

--- CodeExtractor.hs
+++ CodeExtractor.hs
@@ -1,10 +1,20 @@
 module CodeExtractor where
 
 import Data.List
+import Data.Char
 import Control.Monad
+import Control.Monad.IO.Class
 import System.Directory
 
+import Twelf
+
 type Code = [String]
+
+main :: IO ()
+main = do
+    extractAll
+    createAppendix
+    createExample
 
 readCodeFile :: String -> IO Code
 readCodeFile = fmap lines . readFile
@@ -45,3 +55,119 @@ createAppendix = do
     fileNames <- filter ((== "fle.") . take 4 . reverse) . lines <$> readFile "code/sources.cfg"
     tuples <- sequence [readFile ("code/" ++ fileName) >>= \contents -> return (fileName, lines contents) | fileName <- fileNames]
     writeFile "report/code-appendix.tex" $ appendixCode tuples
+
+twelfPath = "/home/mkm/twelfbin/twelf/bin/twelf-server"
+
+createExample :: IO ()
+createExample = runTwelf twelfPath False $ do
+    loadSources
+    [("E", hexp), ("B", bexp)] <- query (TwRaw "trans-hb-exists test5 (_ : trans-hb store/nil B E)")
+    [("P", sprog)] <- query (TwName "trans-bs" `TwApp` bexp `TwApp` TwName "P")
+    [("L", start), ("Q", mprog)] <- query (TwName "trans-sm" `TwApp` sprog `TwApp` TwName "Q" `TwApp` TwName "L")
+    liftIO . writeFile "report/code-example-hoas.tex" . texifyHExp 1 . mkHExp $ hexp
+    liftIO . writeFile "report/code-example-bruijn.tex" . texifyBExp 1 . mkBExp $ bexp
+    liftIO . writeFile "report/code-example-stack.tex" . texifySProg . mkSProg $ sprog
+    liftIO . writeFile "report/code-example-machine.tex" . texifyMProg . mkMProg $ mprog
+
+mkNat :: TwExp -> Integer
+mkNat (TwName "z") = 0
+mkNat (TwApp (TwName "s") e) = 1 + mkNat e
+mkNat (TwName [digit]) | ord '0' <= ord digit && ord digit <= ord '9' = read [digit]
+
+data HExp = HNum Integer | HVar String | HLam String HExp | HApp HExp HExp | HSuc HExp
+    deriving (Show)
+
+mkHExp :: TwExp -> HExp
+mkHExp (TwName "num" `TwApp` e) = HNum $ mkNat e
+mkHExp (TwName name) = HVar (filter (not . isDigit) name)
+mkHExp (TwName "lam" `TwApp` TwLam name _ e) = HLam (filter (not . isDigit) name) $ mkHExp e
+mkHExp (TwName "app" `TwApp` e1 `TwApp` e2) = HApp (mkHExp e1) (mkHExp e2)
+mkHExp (TwName "suc" `TwApp` e) = HSuc $ mkHExp e
+
+texifyHExp :: Int -> HExp -> String
+texifyHExp _ (HNum n) = "\\n{" ++ show n ++ "}"
+texifyHExp _ (HVar name) = name
+texifyHExp 1 (HLam name e) = "\\lam{" ++ name ++ "}{" ++ texifyHExp 1 e ++ "}"
+texifyHExp _ (HLam name e) = "(\\lam{" ++ name ++ "}{" ++ texifyHExp 1 e ++ "})"
+texifyHExp 3 (HApp e1 e2) = "(\\app{" ++ texifyHExp 2 e1 ++ "}{" ++ texifyHExp 3 e2 ++ "})"
+texifyHExp _ (HApp e1 e2) = "\\app{" ++ texifyHExp 2 e1 ++ "}{" ++ texifyHExp 3 e2 ++ "}"
+texifyHExp 3 (HSuc e) = "(\\hsuc{" ++ texifyHExp 3 e ++ "})"
+texifyHExp _ (HSuc e) = "\\hsuc{" ++ texifyHExp 3 e ++ "}"
+
+data BExp = BNum Integer | BVar Integer | BLam BExp | BApp BExp BExp | BSuc BExp
+    deriving (Show)
+
+mkBExp :: TwExp -> BExp
+mkBExp (TwName "bnum" `TwApp` e) = BNum $ mkNat e
+mkBExp (TwName "bvar" `TwApp` e) = BVar $ mkNat e
+mkBExp (TwName "blam" `TwApp` e) = BLam $ mkBExp e
+mkBExp (TwName "bapp" `TwApp` e1 `TwApp` e2) = BApp (mkBExp e1) (mkBExp e2)
+mkBExp (TwName "bsuc" `TwApp` e) = BSuc $ mkBExp e
+
+texifyBExp :: Int -> BExp -> String
+texifyBExp _ (BNum n) = "\\n{" ++ show n ++ "}"
+texifyBExp _ (BVar i) = show i
+texifyBExp 1 (BLam e) = "\\blam{" ++ texifyBExp 1 e ++ "}"
+texifyBExp _ (BLam e) = "(\\blam{" ++ texifyBExp 1 e ++ "})"
+texifyBExp 3 (BApp e1 e2) = "(\\bapp{" ++ texifyBExp 2 e1 ++ "}{" ++ texifyBExp 3 e2 ++ "})"
+texifyBExp _ (BApp e1 e2) = "\\bapp{" ++ texifyBExp 2 e1 ++ "}{" ++ texifyBExp 3 e2 ++ "}"
+texifyBExp 3 (BSuc e) = "(\\bsuc{" ++ texifyBExp 3 e ++ "})"
+texifyBExp _ (BSuc e) = "\\bsuc{" ++ texifyBExp 3 e ++ "}"
+
+data SExp = SNum Integer | SVar Integer | SLam [SExp] | SApp | SSuc
+    deriving (Show)
+
+type SProg = [SExp]
+
+mkSExp :: TwExp -> SExp
+mkSExp (TwName "snum" `TwApp` e) = SNum $ mkNat e
+mkSExp (TwName "svar" `TwApp` e) = SVar $ mkNat e
+mkSExp (TwName "slam" `TwApp` e) = SLam $ mkSProg e
+mkSExp (TwName "sapp") = SApp
+mkSExp (TwName "ssuc") = SSuc
+
+mkSProg :: TwExp -> SProg
+mkSProg (TwName "sprog/nil") = []
+mkSProg (TwName "sprog/cons" `TwApp` e `TwApp` es) = mkSExp e : mkSProg es
+
+texifySExp :: SExp -> String
+texifySExp (SNum n) = "\\n{" ++ show n ++ "}"
+texifySExp (SVar i) = show i
+texifySExp (SLam es) = "(\\slam{" ++ texifySProg es ++ "})"
+texifySExp SApp = "\\sapp"
+texifySExp SSuc = "\\ssuc"
+
+texifySProg :: SProg -> String
+texifySProg = intercalate ";" . map texifySExp
+
+data MExp = MPushNum Integer | MPushVar Integer | MPushClos Integer | MCall Integer | MInc | MRet | MHalt
+    deriving (Show)
+
+type MProg = [MExp]
+
+mkMExp :: TwExp -> MExp
+mkMExp (TwName "mpushnum" `TwApp` e) = MPushNum $ mkNat e
+mkMExp (TwName "mpushvar" `TwApp` e) = MPushVar $ mkNat e
+mkMExp (TwName "mpushclos" `TwApp` e) = MPushClos $ mkNat e
+mkMExp (TwName "mcall" `TwApp` e) = MCall $ mkNat e
+mkMExp (TwName "minc") = MInc
+mkMExp (TwName "mret") = MRet
+mkMExp (TwName "mhalt") = MHalt
+
+mkMProg :: TwExp -> MProg
+mkMProg (TwLam nil _ e) = f e
+    where
+        f (TwName nil') | nil' == nil = []
+        f (TwName "mprog'/cons" `TwApp` e `TwApp` es) = mkMExp e : f es
+
+texifyMExp :: MExp -> String
+texifyMExp (MPushNum n) = "\\mpushnum{" ++ show n ++ "}"
+texifyMExp (MPushVar i) = "\\mpushvar{" ++ show i ++ "}"
+texifyMExp (MPushClos ell) = "\\mpushclos{" ++ show ell ++ "}"
+texifyMExp (MCall ell) = "\\mcall{" ++ show ell ++ "}"
+texifyMExp MInc = "\\minc"
+texifyMExp MRet = "\\mret"
+texifyMExp MHalt = "\\mhalt"
+
+texifyMProg :: MProg -> String
+texifyMProg es = unlines ["\\text{" ++ show i ++ ":}& \\quad" ++ texifyMExp e ++ "\\\\" | (e, i) <- zip es [0 ..]]
---
+++ Twelf.hs
@@ -0,0 +1,118 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+module Twelf where
+
+import Data.List
+import Control.Monad.Reader
+import Text.Parsec
+import System.IO
+import System.Process
+
+newtype Twelf a = Twelf (ReaderT (Handle, Handle, Bool) IO a)
+    deriving (Functor, Applicative, Monad, MonadReader (Handle, Handle, Bool), MonadIO)
+
+runTwelf :: String -> Bool-> Twelf a -> IO a
+runTwelf twelfExe verbose twelf = do
+    let (Twelf m) = waitForOK >> twelf
+    let creator = (proc twelfExe []) {
+            cwd = Just "code",
+            std_in = CreatePipe,
+            std_out = CreatePipe
+        }
+    (Just hin, Just hout, _, hproc) <- createProcess creator
+    hSetBuffering hin LineBuffering
+    hSetBuffering hout LineBuffering
+    x <- runReaderT m (hin, hout, verbose)
+    hClose hin
+    hClose hout
+    waitForProcess hproc
+    return x
+
+drain :: Handle -> IO ()
+drain hndl = do
+    r <- hWaitForInput hndl 100
+    when r (hGetLine hndl >>= print >> drain hndl)
+
+waitFor :: (String -> Bool) -> Twelf ()
+waitFor p = do
+    line <- readLineFromTwelf
+    when (not $ p line) $ waitFor p
+
+waitForOK :: Twelf ()
+waitForOK = waitFor (== "%% OK %%")
+
+readLineFromTwelf :: Twelf String
+readLineFromTwelf = do
+    (_, hout, verbose) <- ask
+    line <- liftIO $ hGetLine hout
+    when verbose $ liftIO . putStrLn $ "Twelf: " ++ line
+    return line
+
+writeLineToTwelf :: String -> Twelf ()
+writeLineToTwelf line = do
+    (hin, _, verbose) <- ask
+    liftIO $ hPutStrLn hin line
+    when verbose $ liftIO . putStrLn $ "Monad: " ++ line
+
+command :: String -> Twelf ()
+command cmd = do
+    writeLineToTwelf cmd
+    waitForOK
+
+loadFile :: String -> Twelf ()
+loadFile fileName = command $ "loadFile " ++ fileName
+
+loadSources :: Twelf ()
+loadSources = command "Config.load"
+
+readSentence :: Twelf String
+readSentence = unlines <$> f
+    where
+        f = do
+            line <- readLineFromTwelf
+            case reverse line of
+                ('.' : _) -> return $ [init line]
+                _ -> (:) line <$> f
+
+query :: TwExp -> Twelf [(String, TwExp)]
+query pattern = do
+    writeLineToTwelf "readDecl"
+    writeLineToTwelf $ "%query 1 1 " ++ show pattern ++ ".%."
+    waitFor $ \line -> take 3 line == "---"
+    result <- readSentence
+    bindings <- case parse pQueryResult "" result of
+        Left err -> error $ show err
+        Right bindings -> return bindings
+    waitForOK
+    return bindings
+
+data TwExp = TwName String | TwApp TwExp TwExp | TwLam String TwExp TwExp | TwRaw String
+
+instance Show TwExp where
+    show (TwName name) = name
+    show (TwApp e1 e2) = "(" ++ show e1 ++ " " ++ show e2 ++ ")"
+    show (TwLam name typ body) = "[" ++ name ++ ":" ++ show typ ++ "]" ++ show body
+    show (TwRaw raw) = "(" ++ raw ++ ")"
+
+type P = Parsec String ()
+
+pQueryResult :: P [(String, TwExp)]
+pQueryResult = do
+    name <- pIdent
+    pSymbol '='
+    value <- pExp
+    (:) (name, value) <$> (pSymbol ';' *> pQueryResult <|> return [])
+
+pExp :: P TwExp
+pExp = foldl1 TwApp <$> many1 pSimpleExp
+
+pSimpleExp :: P TwExp
+pSimpleExp = (TwName <$> pIdent) <|> (pSymbol '(' *> pExp <* pSymbol ')') <|> pLam
+
+pLam :: P TwExp
+pLam = TwLam <$> (pSymbol '[' *> pIdent) <*> (pSymbol ':' *> pExp) <*> (pSymbol ']' *> pExp)
+
+pIdent :: P String
+pIdent = many1 (letter <|> digit <|> oneOf "'-/") <* spaces
+
+pSymbol :: Char -> P ()
+pSymbol symbol = char symbol >> spaces
